2b5a624956533feb07bcd57fadf2cd7a9d1f628c
dagibro4-ux <dagibro4@gmail.com>
2026-07-25 13:08:39 +0300

    Refactor index.html for improved functionality
    Add live viewer count update script to server.js

--- server.js
+++ server.js
@@ -16,3 +16,19 @@ const config = {
 
 var nms = new NodeMediaServer(config);
 nms.run();
+<script>
+    // መጀመሪያ የሚታየው ቁጥር
+    let viewers = 1000; 
+
+    function updateLiveStats() {
+        // በየ 3 ሰከንዱ ከ 1 እስከ 3 ሰው እንዲጨምር የሚያደርግ ኮድ
+        let randomIncrease = Math.floor(Math.random() * 3) + 1;
+        viewers = viewers + randomIncrease;
+        
+        // በገጹ ላይ ቁጥሩን ማዘመን
+        document.getElementById("viewer-count").innerText = viewers;
+    }
+
+    // በየ 3 ሰከንዱ (3000 ሚሊ ሰከንድ) ተግባሩን እንዲደግም ማድረግ
+    setInterval(updateLiveStats, 3000);
+</script>
